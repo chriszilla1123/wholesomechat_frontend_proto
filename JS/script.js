7c7134654a1fc51aff3d3c20503326747725c396
Chris Hill <chriszilla1123@gmail.com>
2017-10-23 16:34:11 -0700

    more spacing

--- JS/script.js
+++ JS/script.js
@@ -13,11 +13,11 @@ $(document).ready(function(){
         toPrint += "Fear: " + data.tone_categories[0].tones[2].score; + "<br>  ";
         toPrint += "Joy: " + data.tone_categories[0].tones[3].score; + "<br>  ";
         toPrint += "Sadness: " + data.tone_categories[0].tones[4].score; + "<br>  ";
-        toPrint += "Language Tones:  <br>  ";
+        toPrint += "<br>Language Tones:  <br>  ";
         toPrint += "Analytical: " + data.tone_categories[1].tones[0].score; + "<br>  ";
         toPrint += "Confident: " + data.tone_categories[1].tones[1].score; + "<br>  ";
         toPrint += "Tentative: " + data.tone_categories[1].tones[2].score; + "<br>  ";
-        toPrint += "Social Tones:  <br>  ";
+        toPrint += "<br>Social Tones:  <br>  ";
         toPrint += "Openness: " + data.tone_categories[2].tones[0].score; + "<br>  ";
         toPrint += "Conscientiousness: " + data.tone_categories[2].tones[1].score; + "<br>  ";
         toPrint += "Extraversion: " + data.tone_categories[2].tones[2].score; + "<br>  ";
