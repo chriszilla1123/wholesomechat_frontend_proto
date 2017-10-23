b27bf42808d427997a05bfb842fd60d4ace41dca
Chris Hill <chriszilla1123@gmail.com>
2017-10-23 16:07:15 -0700

    testing

--- JS/script.js
+++ JS/script.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
   endpoint = "http://chilltec.net:4567/api/getTone";
   $("#submitMessage").on("click", function(){
     userInput = $("#textbox").val();
-    console.log(userInput);
     if (userInput != "" && userInput != undefined) {
       $.post(endpoint, userInput, function(data){
-        $("#response").html(JSON.stringify(data));
-        console.log("Success!");
+        console.log(data);
+        response = JSON.parse(data);
+        toPrint = "";
+        //$("#response").html(JSON.stringify(data));
+        toPring += ""
       });
     }
   });
