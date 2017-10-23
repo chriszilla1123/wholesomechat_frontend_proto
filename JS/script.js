c29867a501082e39f308b39b29c9b296d4731bd3
Chris Hill <chriszilla1123@gmail.com>
2017-10-23 15:47:55 -0700

    fixed api url

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  endpoint = "chilltec.net:4567/api/getTone";
+  endpoint = "http://chilltec.net:4567/api/getTone";
   $("#submitMessage").on("click", function(){
     userInput = $("#textbox").val();
     console.log(userInput);
